51aa9a89f02b4ecaa6a349c01331bc2b7eb4dfa5
MishkaRogachev <mishkarogachev@gmail.ocm>
2015-02-17 16:49:29 +0300

    AbstractInterface of CacheSpace tested

--- core/tests/unit_tests/space_tests/cache_space_test.cpp
+++ core/tests/unit_tests/space_tests/cache_space_test.cpp
@@ -54,14 +54,22 @@ void CacheSpaceTest::testConstructors()
 
 void CacheSpaceTest::testAbstractInterface()
 {
+    SpacePtr mock(new SpaceMock({ { { 12, 34, 1 }, VolumePtr(new Volume(1, 1, 1)) },
+                                  { { 43, -5, 0 }, VolumePtr(new Volume(1, 1, 1)) },
+                                  { { -7, 69, 7 }, VolumePtr(new Volume(1, 1, 1)) }}));
+    CacheSpace space(mock);
 
+    QVERIFY(space.positions().empty());
+    QVERIFY(space.volumes().empty());
+
+    QCOMPARE(space.volumeAt(32, 11, 4), VolumePtr());
+    QVERIFY(space.volumeAt(12, 34, 1) != VolumePtr());
 }
 
 void CacheSpaceTest::testLoading()
 {
     SpacePtr mock(new SpaceMock({ { { 4, 4, -4 }, VolumePtr(new Volume(1, 1, 1)) },
                                   { { 5, -5, 0 }, VolumePtr(new Volume(1, 1, 1)) } }));
-
     CacheSpace space(mock);
 
     QVERIFY(!space.load(0, 1, 2));
